c19b1934d16cbbbccb3c817fe10c101e0f042368
leoalcantara <leonidio.ti@gmail.com>
2020-08-05 19:44:54 -0300

    finalizing the landingpage

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -12,11 +12,12 @@ import './styles.css';
 function Landing(){
   return (
       <div id="page-landing">
-        <div id="page-landing-content" className="container">
+        <div id="page-landing-content" className="container">          
           <div className="logo-container">
             <img src={logoimg} alt="Proffy"/>
             <h2>Sua plataforma de estudos online.</h2>
           </div>
+
           <img 
             src={landingimg} 
             alt="Plataforma de estudos" 
@@ -24,18 +25,18 @@ function Landing(){
           />
 
           <div className="buttons-container">
-            <a href="" className="study">
+            <a href="/" className="study">
               <img src={studyIcon} alt="Estudar"/>
               Estudar
             </a>
 
-            <a href="" className="give-classes">
-              <img src={giveClassesIcon} alt="Estudar"/>
-              Estudar
+            <a href="/" className="give-classes">
+              <img src={giveClassesIcon} alt="Dar aulas"/>
+              Dar aulas
             </a>
           </div>
           <span className="total-connections">
-            Total de 200 conexõesjá realizadas
+            Total de 200 conexões já realizadas
             <img src={purpleHeartIcon} alt="Coração roxo"/>
           </span>
         </div>
