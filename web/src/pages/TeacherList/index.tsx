6cbf4143c0d52e928771231a2eb66129b7818984
leoalcantara <leonidio.ti@gmail.com>
2020-08-07 21:44:39 -0300

    finished the PageHeader component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
 
+import './styles.css';
+import PageHeader from '../../components/PageHeader';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+
 function TeacherList() {
     return (
-       <h1> Teachers</h1>
+       <div id="page-teacher-list" className="container">
+        <PageHeader title="Esses são os proffys disponiveis." > 
+            <form id="search-teachers">
+                <div className="input-block">
+                    <label htmlFor="subject">Matéria</label>
+                    <input type="text" id="subject"/>
+                </div>
+
+                <div className="input-block">
+                    <label htmlFor="week_day">Dia da semana</label>
+                    <input type="text" id="week_day"/>
+                </div>
+
+                <div className="input-block">
+                    <label htmlFor="time">Hora</label>
+                    <input type="text" id="time"/>
+                </div>
+            </form>
+        </PageHeader>
+
+        
+       </div>
     )
 }
 
